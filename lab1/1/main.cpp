c0f02c4fbf8fccbf964b43b5213a252a7eaad580
fit-com <nsufitcommunity@proton.me>
2026-09-20 01:54:21 +0700

    Add main function to process double input
    Implement main function to read a double and compute a result.

--- lab1/1/main.cpp
+++ lab1/1/main.cpp
@@ -1,2 +1,13 @@
+#include <stdio.h>
 
-ыввывпывп
+int main() {
+    double n;
+    scanf("%lf", &n);
+    
+    int res = (int)n;
+    if (n > 0) n = n - res;
+
+    res += (int)(n * 2);
+    printf("%i\n", res);
+    return 0;
+}
