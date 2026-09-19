b66895ed9452972ba085a7d41a6bc0b72b7c2ec7
fit-com <nsufitcommunity@proton.me>
2026-09-20 00:26:40 +0700

    Update main.cpp

--- lab1/1/main.cpp
+++ lab1/1/main.cpp
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    double n;
+    scanf("%lf", &n);
+    
+    int res = (int)n;
+    n = n - res;
+
+    res += (int)(n * 2);
+    printf("%i\n", res);
+    return 0;
+}
